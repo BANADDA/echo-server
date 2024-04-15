80950bb8dd5fb20a939b0023c328f7f51318daea
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-04-15 12:11:38 +0300

    modified:   firebase-config.js 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,23 @@
 const admin = require('firebase-admin');
 
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+  console.error('FIREBASE_PRIVATE_KEY not set in environment.');
+  process.exit(1); // Exit the process if the key is not set.
+}
+
 admin.initializeApp({
   credential: admin.credential.cert({
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure newlines are handled correctly
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
     token_uri: process.env.FIREBASE_TOKEN_URI,
     auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  }),
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  })
 });
 
 const db = admin.firestore();
@@ -36,5 +41,5 @@ async function addTrainingJobMetadata(docId, modelId, datasetId, imageTag, compu
 }
 
 module.exports = {
-    addTrainingJobMetadata
+    db, addTrainingJobMetadata
 };
